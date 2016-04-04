cae5e509ea8460926cffebd6e67f12bf4e250385
erocrizs <aemielvin.loremia@gmail.com>
2016-04-04 11:43:04 +0800

    Implement Vector class

--- Source Code/Vector.java
+++ Source Code/Vector.java
@@ -1,45 +1,54 @@
 public class Vector {
-	private float x;
-	private float y;
+	private double x;
+	private double y;
 	
-	public Vector (int x, int y) {
-		// constructor
+	public Vector (double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
 	
 	public Vector add (Vector that) {
-		// stuff
+		return new Vector(this.x + that.x, this.y + that.y);
 	}
 	
 	public Vector subtract (Vector that) {
-		// stuff
+		return new Vector(this.x - that.x, this.y - that.y);
 	}
 	
-	public Vector scalarMult (float factor) {
-		// stuff
+	public Vector scalarMult (double factor) {
+		return new Vector(this.x * factor, this.y * factor);
 	}
 	
-	public Vector rotate (float degree) {
-		// stuff
+	public Vector rotate (double degree) {
+		double radians =  Math.PI * (degree/180);
+		return new Vector(	(Math.cos(radians)*x - Math.sin(radians)*y),
+							(Math.sin(radians)*x + Math.cos(radians)*y));
+	}
+	
+	public double magnitude() {
+		return  Math.sqrt(x*x + y*y);
 	}
 	
 	public Vector normalize() {
-		// stuff
+		return this.scalarMult( 1 / this.magnitude() );
 	}
 	
 	public Vector negate() {
-		// stuff
+		return this.scalarMult(-1);
+	}
+	
+	public double getAngle() {
+		double radians = Math.atan2(y, x);
+		return 180*(radians/Math.PI);
 	}
 	
 	//getters
 	public int getX() {
-		return (int) x;
+		return (int) Math.round(x);
 	}
 	
 	public int getY() {
-		return (int) y;
+		return (int) Math.round(y);
 	}
 	
-	public float getAngle() {
-		//stuff
-	}
 }
