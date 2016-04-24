1336b8b9c3e68c911d5d1a054a4bdc4f3dc1a636
Jino <breginald.basilio@yahoo.com>
2016-04-25 00:29:52 +0800

    Re-commiting some untouched stuff

--- Source Code/CollideRectangle.java
+++ Source Code/CollideRectangle.java
@@ -1,10 +1,32 @@
-
+/**
+* This class represents a rectangular CollideShape object
+*
+* @see	CollideShape
+*
+* @author Aemielvin Loremia
+*/
 public class CollideRectangle extends CollideShape {
-
+	/**
+	* Creates a new rectangular CollideShape with the specified
+	* position and dimensions
+	* 
+	* @param	position	The position of this instance of
+	*						CollideShape
+	* @param	dimension	The dimensions of this CollideRectangle
+	*/
 	public CollideRectangle(Vector position, Vector dimension) {
 		super(position, dimension);
 	}
 
+	/**
+	* Calculates the shortest distance between this CollideRectangle
+	* and the specified Vector
+	*
+	* @param	external	The Vector from which the distance is
+	*						to be measured
+	* @param	double	The shortest distance between this instance
+	*					of CollideRectangle and the specified Vector
+	*/
 	@Override
 	public double getDistanceFromPointNearestTo(Vector external) {
 		double minX = position.x - (dimension.x/2.0);
--- Source Code/DrawableImage.java
+++ Source Code/DrawableImage.java
@@ -1,18 +1,43 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+/**
+* This class represents an image Drawable
+*
+* @see	Drawable
+*
+* @author	Aemielvin Loremia
+*/
 public class DrawableImage extends Drawable {
-	
+	/*
+	* The BufferedImage to be "converted" to Drawable
+	*/
 	public BufferedImage image;
 	
+	/*
+	* Converts the specified image into a Drawable with the specified
+	* position and dimensions
+	*
+	* @param	position	Coordinates of the position of this
+	* 						Drawable
+	* @param	dimension	The dimensions of this Drawable
+	* @param	image	The BufferedImage to be "converted" to
+	*					Drawable
+	*/
 	public DrawableImage(Vector position, Vector dimension, BufferedImage image) {
 		super(position, dimension);
 		this.image = image;
 	}
 
+	/*
+	* Draws this instance of Drawable
+	*
+	* @param	g	The graphics to be drawn
+	*
+	* @see	Drawable#topLeftPosition
+	*/
 	@Override
 	public void draw(Graphics2D g) {
 		g.drawImage(image, topLeftPosition.getX(), topLeftPosition.getY(), dimension.getX(), dimension.getY(), null);
 	}
-
 }
