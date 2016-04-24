4fabc940fd6c29ea66d7851ce73dfdcedaf84bde
Jino <breginald.basilio@yahoo.com>
2016-04-25 00:32:10 +0800

    More untouched stuff

--- Source Code/PauseState.java
+++ Source Code/PauseState.java
@@ -1,34 +1,21 @@
-/**
-* This class reperesents the pause screen of the games	
-* 
-* 
-* @author	Aemielvin Loremia
-* @author	Ivan Martinez
-*/
-public class Pause extends State {
-	
-	/*
-	Overriden method from the State handleInput
-	Handles all inputs from an InputCollector object, responds with the appropiate method
-	@param input: the InputCollector object to check for mouse and keyboard movement
-	*/
-	protected void handleInput (InputHandler input) {
-		// stuff
+public class PauseState extends State {
+
+
+	@Override
+	public void render(RenderWindow rw) {
+		// TODO Auto-generated method stub
+		
 	}
-	/*
-	Overriden method from the State update
-	Updates the current objects given a value, and adjusts positions/states accordingly
-	@param delta: used to update objects based from time passed
-	*/
-	protected void update (float delta) {
-		// stuff
+
+	@Override
+	public void handleInput(InputCollector input) {
+		// TODO Auto-generated method stub
+		
 	}
-	/*
-	Overriden method from the State render
-	Renders all compatible objects into this window
-	@param rw: the RenderWindow object where objects will be rendered/drawn
-	*/
-	protected void render (Graphics g) {
-		// stuff
+
+	@Override
+	public void update(double delta) {
+		// TODO Auto-generated method stub
+		
 	}
 }
