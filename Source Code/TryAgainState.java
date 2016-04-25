e02984223da199a10754e8398e4d07d086a7e23a
Vaan52 <123zachivanmartinez321@gmail.com>
2016-04-25 11:40:34 +0800

    Revert "Removed TryAgainState again"
    This reverts commit ddca2107921679fd7392fdfb9e845b43d8efdd8e.

---
+++ Source Code/TryAgainState.java
@@ -0,0 +1,13 @@
+public class TryAgain extends State {
+	protected void handleInput (InputHandler input) {
+		// stuff
+	}
+	
+	protected void update (float delta) {
+		// stuff
+	}
+	
+	protected void render (Graphics g) {
+		// stuff
+	}
+}
