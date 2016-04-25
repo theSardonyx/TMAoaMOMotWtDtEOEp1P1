9a61013ff5e36c577cde64015898cfdc21ea3a2a
Vaan52 <123zachivanmartinez321@gmail.com>
2016-04-25 16:26:42 +0800

    Added Enemies

---
+++ Source Code/EyeEnemy.java
@@ -0,0 +1,21 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+public class EyeEnemy extends Entity {
+
+	public EyeEnemy(Vector position, Vector dimension, BulletStage stage) {
+		super(position, dimension, stage);
+		SpriteSheet ss = SpriteSheetLoader.getInstance().getSpriteSheet("res/img/64x64-sheet.png", 64, 64);
+		visual = new Sprite(position, dimension, new BufferedImage[] {
+													ss.get(0, 3), ss.get(2, 3)}, 
+												new BufferedImage[] {
+													ss.get(1, 3), ss.get(3, 3)}, Color.GREEN);
+		((Sprite) visual).setStateRate(1);
+	}
+
+	@Override
+	public void updateHook(double delta) {
+		((Sprite) visual).update(delta);
+	}
+
+}
