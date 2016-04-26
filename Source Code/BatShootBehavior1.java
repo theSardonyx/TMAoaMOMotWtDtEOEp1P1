a283752b7f11b166ce5239c1a08bed0c37e0a6e9
erocrizs <aemielvin.loremia@gmail.com>
2016-04-26 09:12:25 +0800

    Make BatShootBehavior1 shoot in set interval

--- Source Code/BatShootBehavior1.java
+++ Source Code/BatShootBehavior1.java
@@ -2,19 +2,21 @@ import java.awt.Color;
 
 public class BatShootBehavior1 extends ShootBehavior {
 	
-	private boolean shot;
 	private Color color;
+	private double timer;
+	
 	public BatShootBehavior1(Entity subject, BulletStage bulletStage, double fireRate, Color color) {
 		super(subject, fireRate, bulletStage);
 		this.color = color;
-		shot = false;
+		timer = 0;
 	}
 
 	@Override
 	public void shoot(double delta) {
-		if(!shot)
+		timer += delta;
+		if(timer>=fireRate)
 		{
-			shot = true;
+			timer = 0;
 			BatBullet projectile = new BatBullet(subject.getPosition(), bulletStage, color);
 			projectile.setMoveBehavior(new WaveMoveBehavior(projectile, new Vector(0, 150), 50, 1));
 			subject.spawnEntity(projectile);
