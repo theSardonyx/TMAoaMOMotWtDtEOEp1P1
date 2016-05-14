6b1eeb8048e2943e6552734c45cf89a717d5b585
erocrizs <aemielvin.loremia@gmail.com>
2016-05-15 01:16:18 +0800

    Segregated entity types into their own array list

--- Source Code/BulletStage.java
+++ Source Code/BulletStage.java
@@ -11,7 +11,12 @@ public class BulletStage {
 	private DrawableImage visual;
 	private Graphics2D graphics;
 	
-	private ArrayList<Entity> entities;
+	private ArrayList<Entity> ambientEntity;
+	private ArrayList<Entity> allyEntity;
+	private ArrayList<Entity> allyBulletEntity;
+	private ArrayList<Entity> enemyEntity;
+	private ArrayList<Entity> enemyBulletEntity;
+	
 	private Queue<Request> requestQueue;
 	
 	private Player player;
@@ -23,7 +28,12 @@ public class BulletStage {
 		visual = new DrawableImage(position.add( dimension.scalarMult( 0.5 ) ), dimension, image);
 		graphics = (Graphics2D) image.getGraphics();
 		
-		entities = new ArrayList<Entity>();
+		ambientEntity = new ArrayList<Entity>();
+		allyEntity = new ArrayList<Entity>();
+		allyBulletEntity = new ArrayList<Entity>();
+		enemyEntity = new ArrayList<Entity>();
+		enemyBulletEntity = new ArrayList<Entity>();
+		
 		requestQueue = new ArrayDeque<Request>();
 
 		player = new Player(new Vector(200, 200), new Vector(64, 64), this);
@@ -37,15 +47,19 @@ public class BulletStage {
 	}
 	
 	public void handleInput(InputCollector input) {
-		for(Entity e: entities) {
-			e.handleInput(input);
-		}
+		ambientEntity.forEach( e -> e.handleInput(input) );
+		allyEntity.forEach( e -> e.handleInput(input) );
+		allyBulletEntity.forEach( e -> e.handleInput(input) );
+		enemyEntity.forEach( e -> e.handleInput(input) );
+		enemyBulletEntity.forEach( e -> e.handleInput(input) );
 	}
 	
 	public void update(double delta) {
-		for(Entity e: entities) {
-			e.update(delta);
-		}
+		ambientEntity.forEach( e -> e.update(delta) );
+		allyBulletEntity.forEach( e -> e.update(delta) );
+		allyEntity.forEach( e -> e.update(delta) );
+		enemyEntity.forEach( e -> e.update(delta) );
+		enemyBulletEntity.forEach( e -> e.update(delta) );
 		
 		while(!requestQueue.isEmpty()) {
 			Request curr = requestQueue.poll();
@@ -57,9 +71,11 @@ public class BulletStage {
 		graphics.setColor(new Color(0x101010));
 		graphics.fillRect( 0, 0, dimension.getX(), dimension.getY());
 		
-		for(Entity e: entities ) {
-			e.draw(graphics);
-		}
+		ambientEntity.forEach( e -> e.draw(graphics) );
+		allyBulletEntity.forEach( e -> e.draw(graphics) );
+		allyEntity.forEach( e -> e.draw(graphics) );
+		enemyEntity.forEach( e -> e.draw(graphics) );
+		enemyBulletEntity.forEach( e -> e.draw(graphics) );
 		
 		rw.draw( visual );
 	}
@@ -69,10 +85,44 @@ public class BulletStage {
 	}
 	
 	public void addEntity(Entity entity) {
-		entities.add( entity );
+		switch(entity.getType())
+		{
+			case Entity.AMBIENT_TYPE:
+				ambientEntity.add(entity);
+				break;
+			case Entity.ALLY_TYPE:
+				allyEntity.add(entity);
+				break;
+			case Entity.ALLY_BULLET_TYPE:
+				allyBulletEntity.add(entity);
+				break;
+			case Entity.ENEMY_TYPE:
+				enemyEntity.add(entity);
+				break;
+			case Entity.ENEMY_BULLET_TYPE:
+				enemyBulletEntity.add(entity);
+				break;
+		}
 	}
 	
 	public void removeEntity(Entity entity) {
-		entities.remove( entity );
+		switch(entity.getType())
+		{
+			case Entity.AMBIENT_TYPE:
+				ambientEntity.remove(entity);
+				break;
+			case Entity.ALLY_TYPE:
+				allyEntity.remove(entity);
+				break;
+			case Entity.ALLY_BULLET_TYPE:
+				allyBulletEntity.remove(entity);
+				break;
+			case Entity.ENEMY_TYPE:
+				enemyEntity.remove(entity);
+				break;
+			case Entity.ENEMY_BULLET_TYPE:
+				enemyBulletEntity.remove(entity);
+				break;
+		}
 	}
 }
