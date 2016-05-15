81af6c266f7620251e2a6c020fefcc01d116f563
EroCrizs <aemielvin.loremia@gmail.com>
2016-05-16 03:32:56 +0800

    Modify WitchBullet constructor to match BatBullet

--- Source Code/WitchBullet.java
+++ Source Code/WitchBullet.java
@@ -2,10 +2,10 @@ import java.awt.Color;
 
 public class WitchBullet extends Entity {
 
-	public WitchBullet(Vector position, Vector dimension, BulletStage stage) {
+	public WitchBullet(Vector position, Vector dimension, BulletStage stage, Color color) {
 		super(position, dimension, stage);
 		SpriteSheet ss = SpriteSheetLoader.getInstance().getSpriteSheet("res/img/enemyBulletSheet.png", 32, 32);
-		this.visual = new DrawableImage(position, dimension, Sprite.integrateSprites(ss.get(0, 4), ss.get(1, 4), Color.GREEN));
+		this.visual = new DrawableImage(position, dimension, Sprite.integrateSprites(ss.get(0, 4), ss.get(1, 4), color));
 		
 		//TODO damge pls
 		this.damage = 1;
