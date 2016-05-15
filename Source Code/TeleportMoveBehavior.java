5e65a4a77b7c8c6a4265cdc8461b6d3c8b433061
erocrizs <aemielvin.loremia@gmail.com>
2016-05-16 02:08:56 +0800

    Having an anchor now an option for teleport move behavior

--- Source Code/TeleportMoveBehavior.java
+++ Source Code/TeleportMoveBehavior.java
@@ -2,14 +2,26 @@
 public class TeleportMoveBehavior extends MoveBehavior {
 
 	private Vector targetPosition;
+	private Entity anchor;
 	
 	public TeleportMoveBehavior(Entity subject, Vector targetPosition) {
 		super(subject, 0.01);
 		this.targetPosition = targetPosition;
+		this.anchor = null;
+	}
+	
+	public TeleportMoveBehavior(Entity subject, Entity anchor)
+	{
+		super(subject, 0.01);
+		this.anchor = anchor;
+		this.targetPosition = this.anchor.getPosition();
 	}
 
 	@Override
 	protected void moveHook(double delta) {
+		if(this.anchor != null)
+			targetPosition = this.anchor.getPosition();
+		
 		subject.setPosition(targetPosition);
 	}
 
