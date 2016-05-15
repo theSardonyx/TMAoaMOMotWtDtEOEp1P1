f2eb769f284a66805f106b5fabd0ea0dceec10b9
EroCrizs <aemielvin.loremia@gmail.com>
2016-05-16 03:13:44 +0800

    Modify SpiderBullet constructor to match BatBullet

--- Source Code/SpiderBullet.java
+++ Source Code/SpiderBullet.java
@@ -2,10 +2,10 @@ import java.awt.Color;
 
 public class SpiderBullet extends Entity {
 
-	public SpiderBullet(Vector position, Vector dimension, BulletStage stage) {
-		super(position, dimension, stage);
+	public SpiderBullet(Vector position, BulletStage stage, Color color) {
+		super(position, new Vector(32, 32), stage);
 		SpriteSheet ss = SpriteSheetLoader.getInstance().getSpriteSheet("res/img/enemyBulletSheet.png", 32, 32);
-		this.visual = new DrawableImage(position, dimension, Sprite.integrateSprites(ss.get(0, 3), ss.get(1, 3), Color.GREEN));
+		this.visual = new DrawableImage(position, dimension, Sprite.integrateSprites(ss.get(0, 3), ss.get(1, 3), color));
 		
 		//TODO damage
 		this.damage = 1;
