82368906326516bceb7eb1ae5bffa4bea89765ae
erocrizs <aemielvin.loremia@gmail.com>
2016-05-16 01:03:19 +0800

    Fix pixie bullet constructor to match bat bullet

--- Source Code/PixieBullet.java
+++ Source Code/PixieBullet.java
@@ -2,13 +2,13 @@ import java.awt.Color;
 
 public class PixieBullet extends Entity {
 
-	public PixieBullet(Vector position, Vector dimension, BulletStage stage) {
-		super(position, dimension, stage);
-		SpriteSheet ss = SpriteSheetLoader.getInstance().getSpriteSheet("res/img/enemyBulletSheet.png", 32, 32);
-		this.visual = new DrawableImage(position, dimension, Sprite.integrateSprites(ss.get(0, 2), ss.get(1, 2), Color.GREEN));
+	public PixieBullet(Vector position, BulletStage stage, Color color) {
+		super(position, new Vector(32, 32), stage);
+		SpriteSheet ss = SpriteSheetLoader.getInstance().getSpriteSheet("res/img/enemyBulletSheet.PNG", 32, 32);
+		this.visual = new DrawableImage(position, dimension, Sprite.integrateSprites(ss.get(0, 2), ss.get(1, 2), color));
 		
 		//TODO damage pls
-		this.damage = 1;
+		this.damage = 1; 
 		
 		this.type = Entity.ENEMY_BULLET_TYPE;
 		this.canCollideAlly = true;
