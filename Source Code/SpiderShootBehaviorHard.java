c3cf234d8e83d1ccc2462796d5ca0551ee456597
Vaan52 <123zachivanmartinez321@gmail.com>
2016-05-16 19:39:09 +0800

    Make SpiderShootBehaviors flexible

--- Source Code/SpiderShootBehaviorHard.java
+++ Source Code/SpiderShootBehaviorHard.java
@@ -2,9 +2,9 @@ import java.awt.Color;
 
 public class SpiderShootBehaviorHard extends ShootBehavior {
 
-	private  Color color;
-	private double accMagnitude;
-	private double initialOffset;
+	private Color color;
+	private double speed, acceleration, offset, waitTime;
+	private int numBullets;
 	
 	public SpiderShootBehaviorHard(Entity subject, BulletStage stage, Color color) {
 		super(subject, stage);
@@ -12,25 +12,46 @@ public class SpiderShootBehaviorHard extends ShootBehavior {
 		this.fireRate = 1;
 		
 		this.color = color;
-		this.accMagnitude = 200;
-		this.initialOffset = 50;
+		this.speed = 0;
+		this.acceleration = 200;
+		this.offset = 50;
+		this.waitTime = 0.75;
+		this.numBullets = 3;
+	}
+	
+	public SpiderShootBehaviorHard(Entity subject, BulletStage stage, Color color, double expireTime) {
+		super(subject, stage, expireTime);
+		
+		this.fireRate = 1;
+		
+		this.color = color;
+		this.speed = 0;
+		this.acceleration = 200;
+		this.offset = 50;
+		this.waitTime = 0.75;
+		this.numBullets = 3;
 	}
 
 	@Override
 	public Entity[] getBullets() {
-		Entity[] bullets = new Entity[3];
+		Entity[] bullets = new Entity[this.numBullets];
 		
-		Vector baseExplodePosition = new Vector(initialOffset, 0);
-		for(int i=0; i<3; i++)
+		Vector baseExplodePosition = new Vector(offset, 0);
+		for(int i=0; i<this.numBullets; i++)
 		{
-			Vector explodePosition = baseExplodePosition.rotate(Math.random()* 2 *Math.PI);
+			Vector explodePosition = baseExplodePosition.rotate(Math.random()* 2 * Math.PI);
 			SpiderBullet projectile = new SpiderBullet(this.subject.getPosition(), this.stage, this.color);
-			projectile.setMoveBehavior(new QueueMoveBehavior(projectile, new MoveBehavior[]{
-					new AccelerateMoveBehavior(projectile, Vector.zero(), Vector.zero(), 0.5),
-					new TimedGlideMoveBehavior(projectile, this.subject.getPosition().add(explodePosition), 0.5, 0.5),
-					new AccelerateMoveBehavior(projectile, Vector.zero(), Vector.zero(), 0.5),
-					new AccelerateMoveBehavior(projectile, Vector.zero(), new Vector(0, 200))
-			}));
+			QueueMoveBehavior queue = new QueueMoveBehavior(projectile, null);
+			
+			AccelerateMoveBehavior mb1 = new AccelerateMoveBehavior(projectile, Vector.zero(), Vector.zero(), this.waitTime);
+			TimedGlideMoveBehavior mb2 = new TimedGlideMoveBehavior(projectile, this.subject.getPosition().add(explodePosition), .25, .25);
+			AccelerateMoveBehavior mb3 = new AccelerateMoveBehavior(projectile, Vector.zero(), Vector.zero(), 0.25);
+			AccelerateMoveBehavior mb4 = new AccelerateMoveBehavior(projectile, new Vector(0, this.speed), new Vector(0, this.acceleration));
+			queue.pushBack(mb1);
+			queue.pushBack(mb2);
+			queue.pushBack(mb3);
+			queue.pushBack(mb4);
+			projectile.setMoveBehavior(queue);
 			
 			bullets[i] = projectile;
 		}
@@ -38,4 +59,24 @@ public class SpiderShootBehaviorHard extends ShootBehavior {
 		return bullets;
 	}
 
+	
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
+	}
+	
+	public void setOffset(double offset) {
+		this.offset = offset;
+	}
+	
+	public void setNumBullets(int numBullets) {
+		this.numBullets = numBullets;
+	}
+	
+	public void setWaitTime(double waitTime) {
+		this.waitTime = waitTime;
+	}
 }
--- Source Code/SpiderShootBehaviorMedium.java
+++ Source Code/SpiderShootBehaviorMedium.java
@@ -2,8 +2,8 @@ import java.awt.Color;
 
 public class SpiderShootBehaviorMedium extends ShootBehavior {
 
-	private  Color color;
-	private double accMagnitude;
+	private Color color;
+	private double speed, acceleration, waitTime;
 	
 	public SpiderShootBehaviorMedium(Entity subject, BulletStage stage, Color color) {
 		super(subject, stage);
@@ -11,7 +11,20 @@ public class SpiderShootBehaviorMedium extends ShootBehavior {
 		this.fireRate = 0.5;
 		
 		this.color = color;
-		this.accMagnitude = 150;
+		this.acceleration = 150;
+		this.speed = 0;
+		this.waitTime = 1.5;
+	}
+	
+	public SpiderShootBehaviorMedium(Entity subject, BulletStage stage, Color color, double expireTime) {
+		super(subject, stage, expireTime);
+		
+		this.fireRate = 0.5;
+		
+		this.color = color;
+		this.acceleration = 150;
+		this.speed = 0;
+		this.waitTime = 1.5;
 	}
 
 	@Override
@@ -19,14 +32,28 @@ public class SpiderShootBehaviorMedium extends ShootBehavior {
 		Entity[] bullets = new Entity[1];
 		
 		SpiderBullet projectile = new SpiderBullet(this.subject.getPosition(), this.stage, this.color);
-		projectile.setMoveBehavior(new QueueMoveBehavior(projectile, new MoveBehavior[]{
-				new AccelerateMoveBehavior(projectile, Vector.zero(), Vector.zero(), 1.5),
-				new AccelerateMoveBehavior(projectile, Vector.zero(), new Vector(0, this.accMagnitude))
-		}));
+		QueueMoveBehavior queue = new QueueMoveBehavior(projectile, null);
+		
+		AccelerateMoveBehavior mb1 = new AccelerateMoveBehavior(projectile, Vector.zero(), Vector.zero(), this.waitTime);
+		AccelerateMoveBehavior mb2 = new AccelerateMoveBehavior(projectile, new Vector(0, this.speed), new Vector(0, this.acceleration));
+		queue.pushBack(mb1);
+		queue.pushBack(mb2);
+		projectile.setMoveBehavior(queue);
 		
 		bullets[0] = projectile;
 		
 		return bullets;
 	}
-
+	
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
+	}
+	
+	public void setWaitTime(double waitTime) {
+		this.waitTime = waitTime;
+	}
 }
