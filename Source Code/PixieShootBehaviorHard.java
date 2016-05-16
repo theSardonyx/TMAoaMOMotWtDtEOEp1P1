98346482070ee810cb02691f5f783a79858f964b
Vaan52 <123zachivanmartinez321@gmail.com>
2016-05-16 15:29:39 +0800

    PixieShootBehaviorHard uses easy and medium level

--- Source Code/PixieShootBehaviorHard.java
+++ Source Code/PixieShootBehaviorHard.java
@@ -1,55 +1,27 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
-public class PixieShootBehaviorHard extends ShootBehavior {
+public class PixieShootBehaviorHard extends CombinationShootBehavior {
 
 	private Color color;
 	private Entity target;
-	private double speed;
-	private double interval;
 	
 	public PixieShootBehaviorHard(Entity subject, Entity target, BulletStage stage, Color color) {
-		super(subject, stage);
-
-		this.fireRate = 1.75;
+		super(subject, stage, null);
 		
+		this.fireRate = 0.1;
 		this.target = target;
-		this.speed = 200;
-		this.interval = 0.25;
 		this.color = color;
-	}
-
-	@Override
-	public Entity[] getBullets() {
-		int basis = (int)(this.fireRate/this.interval);
-		Entity[] bullets = new Entity[basis*2];
 		
-		Vector base = target.getPosition().subtract( subject.getPosition() ).normalize().scalarMult( this.speed );
-		double baseAngle = base.getAngle();
-		double anchorAngle = (3*Math.PI/4);
-		for(int i=0; i<basis; i++)
-		{
-			PixieBullet projectile = new PixieBullet(this.subject.getPosition(), this.stage, this.color);
-			double rotation = baseAngle+anchorAngle - (i * (3*Math.PI/2)/(basis-1));
-			Vector velocity = base.setAngle(rotation);
-			projectile.setMoveBehavior(new QueueMoveBehavior(this.subject, new MoveBehavior[] {
-					new InactiveMoveBehavior(projectile, i*this.interval),
-					new TeleportMoveBehavior(projectile, this.subject),
-					new AccelerateMoveBehavior(projectile, velocity, Vector.zero())
-			}));
-			bullets[i] = projectile;
-			
-			projectile = new PixieBullet(this.subject.getPosition(), this.stage, this.color);
-			rotation = (baseAngle-anchorAngle) + (i * (3*Math.PI/2)/(basis-1));
-			velocity = base.setAngle(rotation);
-			projectile.setMoveBehavior(new QueueMoveBehavior(this.subject, new MoveBehavior[] {
-					new InactiveMoveBehavior(projectile, i*this.interval),
-					new TeleportMoveBehavior(projectile, this.subject),
-					new AccelerateMoveBehavior(projectile, velocity, Vector.zero())
-			}));
-			bullets[basis+i] = projectile;
-		}
+		boolean orientationToRight = Math.random() > 0.5;
+		PixieShootBehaviorEasy sbEasy = new PixieShootBehaviorEasy(this.subject, this.target, this.stage, orientationToRight, color);
+		sbEasy.setBounce(true);
+		sbEasy.setBounceCap(Math.PI / 2);
+		sbEasy.setRotation(this.target.position.subtract(this.subject.position).getAngle());
+		sbEasy.rotate(Math.PI / 4);
+		this.add(sbEasy);
 		
-		return bullets;
+		PixieShootBehaviorMedium sbMedium = new PixieShootBehaviorMedium(this.subject, this.target, this.stage, color);
+		this.add(sbMedium);
 	}
-
 }
