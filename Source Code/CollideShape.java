4af225ef5eb57f864605cedac9fc5d1ffc8c9633
Vaan52 <123zachivanmartinez321@gmail.com>
2016-05-17 16:46:52 +0800

    CollideShape collides ellipses and rectangles

--- Source Code/CollideShape.java
+++ Source Code/CollideShape.java
@@ -1,3 +1,6 @@
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+
 /**
 * This class is a template for CollideShape objects that checks
 * whether they are overlapping / colliding with another
@@ -7,11 +10,14 @@
 * 
 * @author	Aemielvin Loremia
 */
-public abstract class CollideShape {
-	/**
-	* The position of this instance of CollideShape
-	*/
-	protected Vector position;
+public class CollideShape {
+	
+	protected Rectangle2D.Double collideRectangle;
+	protected Ellipse2D.Double collideEllipse;
+	
+	protected boolean canCollideRectangle, canCollideEllipse;
+	
+	protected Vector position, offset;
 	
 	/**
 	* The dimensions of this instance of CollideShape
@@ -28,56 +34,76 @@ public abstract class CollideShape {
 	* @see	#setPosition
 	*/
 	public CollideShape(Vector position, Vector dimension) {
-		this.setPosition(position);
-		this.dimension = dimension;
-	}
-	
-	/**
-	* An abstract method for getting the shortest distance between
-	* the specified Vector and this instance of CollideShape
-	*
-	* @param	external	The Vector from which the distance
-	*						is to be measured
-	* @return	double	The shortest distance between this instance
-	*					of CollideShape and the specified Vector
-	*/
-	public abstract double getDistanceFromPointNearestTo(Vector external);
-	
-	/**
-	* Checks if this instance of CollideShape is overlapping / 
-	* colliding with the specified CollideShape
-	*
-	* @param	p	The CollideShape with which to check if this
-	*				instance is colliding
-	* @return	true	If this instance is colliding with the
-	*					specified CollideShape, or
-	*			false	If they are not
-	*
-	* @see #getDistanceFromPointNearestTo
-	*/
-	public final boolean isCollidingWith(CollideShape p) {
-		double myDistance = getDistanceFromPointNearestTo(p.getPosition());
-		double pDistance = p.getDistanceFromPointNearestTo(position);
-		double actualDistance = position.subtract( p.getPosition() ).magnitude();
-		
-		return ( myDistance + pDistance >= actualDistance );
-	}
-
-	/**
-	* Gets the position of this CollideShape
-	* 
-	* @return	#position
-	*/
-	public Vector getPosition() {
-		return position;
-	}
-
-	/**
-	* Sets the position of this CollideShape
-	*
-	* @param	position	Coordinates of the new position
-	*/
-	public void setPosition(Vector position) {
 		this.position = position;
+		this.dimension = dimension;
+		this.offset = Vector.zero();
+		this.collideRectangle = new Rectangle2D.Double(this.position.x - (this.dimension.x / 2), 
+													this.position.y - (this.dimension.y / 2), 
+													this.dimension.x, this.dimension.y);
+		this.collideEllipse = new Ellipse2D.Double(this.position.x - (this.dimension.x / 2), 
+												this.position.y - (this.dimension.y / 2), 
+												this.dimension.x, this.dimension.y);
+		this.canCollideEllipse = false;
+		this.canCollideRectangle = false;
+	}
+	
+	public CollideShape(Vector position, Vector offset, Vector dimension) {
+		this.position = position;
+		this.dimension = dimension;
+		this.offset = offset;
+		this.collideRectangle = new Rectangle2D.Double(this.position.x + this.offset.x - (this.dimension.x / 2), 
+													this.position.y + this.offset.y - (this.dimension.y / 2), 
+													this.dimension.x, this.dimension.y);
+		this.collideEllipse = new Ellipse2D.Double(this.position.x + this.offset.x - (this.dimension.x / 2), 
+												this.position.y + this.offset.y - (this.dimension.y / 2), 
+												this.dimension.x, this.dimension.y);
+		this.canCollideEllipse = false;
+		this.canCollideRectangle = false;
+	}
+	
+	public final boolean isCollidingWith(CollideShape other) {
+		if(other == null)
+			return false;
+		if(canCollideRectangle) {
+			if(this.getRectangle2D().getBounds2D().intersects(other.getRectangle2D().getBounds2D()))
+				return true;
+			if(this.getRectangle2D().getBounds2D().intersects(other.getEllipse2D().getBounds2D()))
+				return true;
+		}
+		if(canCollideEllipse) {			
+			if(this.getEllipse2D().getBounds2D().intersects(other.getRectangle2D().getBounds2D()))
+				return true;
+			if(this.getEllipse2D().getBounds2D().intersects(other.getEllipse2D().getBounds2D()))
+				return true;
+		}
+		return false;
+	}
+	
+	public Rectangle2D.Double getRectangle2D() {
+		return this.collideRectangle;
+	}
+	
+	public Ellipse2D.Double getEllipse2D() {
+		return this.collideEllipse;
+	}
+	
+	public CollideShape setCollideRectangle(boolean canCollideRectangle) {
+		this.canCollideRectangle = canCollideRectangle;
+		return this;
+	}
+	
+	public CollideShape setCollideEllipse(boolean canCollideEllipse) {
+		this.canCollideEllipse = canCollideEllipse;
+		return this;
+	}
+	
+	public CollideShape updatePosition(Vector v) {
+		this.collideRectangle.x = v.x + this.offset.x - (this.dimension.x / 2);
+		this.collideRectangle.y = v.y + this.offset.y - (this.dimension.y / 2);
+
+		this.collideEllipse.x = v.x + this.offset.x - (this.dimension.x / 2);
+		this.collideEllipse.y = v.y + this.offset.y - (this.dimension.y / 2);
+		
+		return this;
 	}
 }
