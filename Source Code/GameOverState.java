2315028982dba7bbc02eecfba7b6fd997406f3aa
Jino <breginald.basilio@yahoo.com>
2016-05-20 15:43:23 +0800

    Game Over state implemented
    Included some testing phases on Runner.

--- Source Code/GameOverState.java
+++ Source Code/GameOverState.java
@@ -1,20 +1,64 @@
+import java.awt.*;
 public class GameOverState extends State {
-
+        //int currentScore = PlayerSettings.getInstance().getScore();
+        int currentScore = 1000; //testing
+        //int highScore = PlayerSettings.getInstance().getHighScore();
+        int highScore = 500; //testing
+        int ctr = 0;
+        DrawableString decoration1, decoration2, totalLabel, total, newhighnotif;
+        DrawableButton mainMenu, retry;
+        private Vector center = new Vector((Runner.RES_WIDTH / 2), (Runner.RES_HEIGHT / 2));
+        private String deco = "---------------------------------------------------------------------------------------";
+        public GameOverState()
+        {
+            Font font = FontLoader.getInstance().getFont("Press Start 2P", Font.PLAIN, 20);
+            Vector padding = new Vector(10, 10);
+            
+            decoration1 = new DrawableString(new Vector(center.x, center.y -200), deco, font, Color.WHITE);
+            decoration2 = new DrawableString(new Vector(center.x, center.y + 100), deco, font, Color.WHITE);
+            totalLabel = new DrawableString(new Vector(center.x - 150, center.y - 100), "Current Score", font, Color.WHITE);
+            total = new DrawableString(new Vector(center.x + 100, center.y - 100), "lll", font, Color.WHITE);
+            newhighnotif = new DrawableString(new Vector(center.x + 320, center.y - 100), "NEW HIGH SCORE", font, Color.YELLOW);
+            
+            mainMenu = new DrawableButton(new Vector(center.x - 150, center.y + 150),"Back to main menu", font, padding, Color.WHITE);
+            retry = new DrawableButton(new Vector(center.x + 150, center.y + 150), "Retry", font, padding, Color.WHITE);
+        }
 	@Override
 	public void handleInput(InputCollector input) {
-		// TODO Auto-generated method stub
-		
+            mainMenu.update(input.getMousePosition());
+            retry.update(input.getMousePosition());
+            
+            if(input.getMouseReleased(InputCollector.MOUSE_BUTTON1))
+            {
+                if(mainMenu.isCollidingWith(input.getMousePosition()))
+                {
+                    sm.push(Runner.MENU_STATE, null);
+                }
+                else if(retry.isCollidingWith(input.getMousePosition()))
+                {
+                    sm.push(Runner.GAME_STATE, null);
+                }
+                else
+                {
+                    ctr = currentScore;
+                }
+            }
 	}
 
 	@Override
 	public void update(double delta) {
-		// TODO Auto-generated method stub
-		
+            total.setText(Integer.toString(ctr));
+            if(ctr < currentScore) ctr++;
 	}
 
 	@Override
 	public void render(RenderWindow rw) {
-		// TODO Auto-generated method stub
-		
+            rw.draw(decoration1);
+            rw.draw(decoration2);
+            rw.draw(totalLabel);
+            rw.draw(total);
+            if(ctr >= highScore) rw.draw(newhighnotif);
+            rw.draw(mainMenu);
+            rw.draw(retry);
 	}
 }
