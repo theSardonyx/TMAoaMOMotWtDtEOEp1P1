5104a57e7c6cc7a6b8b0bbf252693363dfb12d84
theSardonyx <kryzldeannep@gmail.com>
2016-05-20 22:14:00 +0800

    Reversed the direction of background movement

--- Source Code/Background.java
+++ Source Code/Background.java
@@ -26,7 +26,7 @@ public class Background extends Drawable {
 		this.y += 1;
 		for (int i = 0; i < 24; i++) {
 			for (int j = 0; j < 23; j++) {
-				background[i][j] = new DrawableImage (new Vector (j * 32, 732 - (y % 32) - (i * 32)), new Vector (32, 32), ImageLoader.getInstance().getFile("res/img/tile.png"));
+				background[i][j] = new DrawableImage (new Vector (j * 32, 704 + (y % 32) - (i * 32)), new Vector (32, 32), ImageLoader.getInstance().getFile("res/img/tile.png"));
 			}
 		}
 	}
