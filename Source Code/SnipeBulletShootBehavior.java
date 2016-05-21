b02d21da4b0afd32e02814adc988c483ff6f9eac
Vaan52 <123zachivanmartinez321@gmail.com>
2016-05-21 22:38:40 +0800

    Fix SnipeBulletShootBehavior

--- Source Code/SnipeBulletShootBehavior.java
+++ Source Code/SnipeBulletShootBehavior.java
@@ -1,16 +1,22 @@
 
 public class SnipeBulletShootBehavior extends UpgradableShootBehavior {
 	
+	private double speed;
+	
 	public SnipeBulletShootBehavior(Entity subject, BulletStage stage) {
 		super(subject, stage);
 
 		this.fireRate = 1;
+		
+		this.speed = 3000;
 	}
 	
 	public SnipeBulletShootBehavior(Entity subject, BulletStage stage, double expireTime) {
 		super(subject, stage, expireTime);
 
 		this.fireRate = 1;
+		
+		this.speed = 3000;
 	}
 
 	@Override
@@ -22,8 +28,8 @@ public class SnipeBulletShootBehavior extends UpgradableShootBehavior {
 	@Override
 	public Entity[] getBullets() {
 		SnipeBullet projectile = new SnipeBullet(this.subject.position, this.stage);
-		//HomingMoveBehavior mb = new HomingMoveBehavior(projectile, this.stage);
-		//projectile.setMoveBehavior(mb);
+		AimedMoveBehavior mb = new AimedMoveBehavior(projectile, this.stage.getRandomType(Entity.ENEMY_TYPE), this.speed, 0);
+		projectile.setMoveBehavior(mb);
 		
 		Entity[] bullets = new Entity[1];
 		bullets[0] = projectile;
