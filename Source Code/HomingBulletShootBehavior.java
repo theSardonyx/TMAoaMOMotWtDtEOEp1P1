c81aa34d4b02616c493d6065b42525cc1a8d94ba
Vaan52 <123zachivanmartinez321@gmail.com>
2016-05-21 22:37:30 +0800

    Fix HomingBulletShootBehavior

--- Source Code/HomingBulletShootBehavior.java
+++ Source Code/HomingBulletShootBehavior.java
@@ -1,18 +1,31 @@
 
 public class HomingBulletShootBehavior extends UpgradableShootBehavior {
 
-	private Vector velocity, acceleration;
+	private double speed, acceleration, angularInterval;
+	private int numBullets;
 	
 	public HomingBulletShootBehavior(Entity subject, BulletStage stage) {
 		super(subject, stage);
 
-		this.fireRate = 1;
+		this.fireRate = 3;
+		
+		this.speed = 200;
+		this.acceleration = 0;
+		this.numBullets = 4;
+		
+		this.angularInterval = Math.PI / 18;
 	}
 	
 	public HomingBulletShootBehavior(Entity subject, BulletStage stage, double expireTime) {
 		super(subject, stage, expireTime);
 
 		this.fireRate = 1;
+		
+		this.speed = 200;
+		this.acceleration = 0;
+		this.numBullets = 4;
+
+		this.angularInterval = Math.PI / 18;
 	}
 
 	@Override
@@ -23,15 +36,26 @@ public class HomingBulletShootBehavior extends UpgradableShootBehavior {
 
 	@Override
 	public Entity[] getBullets() {
-		Entity target = null; //TODO get target
-		HomingBullet projectile = new HomingBullet(this.subject.position, this.stage, target);
-		this.velocity = Vector.zero(); //TODO get velocity going to target
-		this.acceleration = new Vector(0, -100); //TODO get acceleration going to target
-		AccelerateMoveBehavior mb = new AccelerateMoveBehavior(projectile, this.velocity.clone(), this.acceleration.clone());
-		projectile.setMoveBehavior(mb);
-		
-		Entity[] bullets = new Entity[1];
-		bullets[0] = projectile;
+		Entity[] bullets = new Entity[numBullets];
+		double baseInterval = this.angularInterval / 2.0;
+		for(int i = 0; numBullets / 2 > i; i++) {
+			double rot1 = baseInterval + (this.angularInterval * i);
+			HomingBullet projectile1 = new HomingBullet(this.subject.position, this.stage);
+			((DrawableImage) projectile1.visual).setRotation(rot1);
+			HomingMoveBehavior mb1 = new HomingMoveBehavior(projectile1, this.stage.getRandomType(Entity.ENEMY_TYPE), new Vector(0, -speed).rotate(rot1), 2 * Math.PI, 0);
+			projectile1.setMoveBehavior(mb1);
+			
+			double rot2 = - baseInterval - (this.angularInterval * i);
+			HomingBullet projectile2 = new HomingBullet(this.subject.position, this.stage);
+			((DrawableImage) projectile2.visual).setRotation(rot2);
+			HomingMoveBehavior mb2 = new HomingMoveBehavior(projectile2, this.stage.getRandomType(Entity.ENEMY_TYPE), new Vector(0, -speed).rotate(rot2), 2 * Math.PI, 0);
+			projectile2.setMoveBehavior(mb2);
+			
+			bullets[(i * 2)] = projectile1;
+			bullets[(i * 2) + 1] = projectile2;
+		}
 		return bullets;
 	}
+	
+	
 }
