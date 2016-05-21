596c9fd360f245b3ff2253fd4b15ad8906876801
Vaan52 <123zachivanmartinez321@gmail.com>
2016-05-21 22:35:25 +0800

    Make EnemyEntity abstract class

---
+++ Source Code/EnemyEntity.java
@@ -0,0 +1,35 @@
+
+public abstract class EnemyEntity extends Entity {
+	
+	protected double heartChance;
+
+	public EnemyEntity(Vector position, Vector dimension, BulletStage stage) {
+		super(position, dimension, stage);
+		
+		this.type = Entity.ENEMY_TYPE;
+		
+		this.damage = 1;
+		
+		this.heartChance = 10;
+	}
+	
+	public EnemyEntity(Vector position, Vector dimension, BulletStage stage, double expireTime) {
+		super(position, dimension, stage, expireTime);
+		
+		this.type = Entity.ENEMY_TYPE;
+		
+		this.damage = 1;
+		
+		this.heartChance = 10;
+	}
+	
+	@Override
+	public void deathAction() {
+		if(Math.random() * 100 <= heartChance) {
+			Heart heart = new Heart(this.position, this.stage);
+			AccelerateMoveBehavior mb = new AccelerateMoveBehavior(heart, Vector.zero(), new Vector(0, 100), 10);
+			heart.setMoveBehavior(mb);
+			this.spawnEntity(heart);
+		}
+	}
+}
